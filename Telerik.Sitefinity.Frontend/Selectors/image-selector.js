f337d3dc87021bac768e8514a270aa5f19907f26
osmak <iosmak@outlook.com>
2014-10-17 18:44:40 +0300

    Added the footer buttons to Image Selector and implemented the Cancel button, which closes the modal dialog.

--- Telerik.Sitefinity.Frontend/Selectors/image-selector.js
+++ Telerik.Sitefinity.Frontend/Selectors/image-selector.js
@@ -2,6 +2,25 @@
 
     var selectors = angular.module('selectors');
 
+    /*
+     * The main controller of the Image Selector.
+     */
+    selectors.controller('sfImageSelectorCtrl', ['$scope', function ($scope) {
+
+        $scope.cancel = function () {
+            $scope.$modalInstance.close();
+        };
+
+        $scope.saveChanges = function () {
+
+        };
+
+    }]);
+
+    /*
+     * Image Selector Modal directive wraps the Image selector in a modal dialog. It is a 
+     * convenience thin wrapper which can be used to open the Image Selector in a modal dialog.
+     */
     selectors.directive('sfImageSelectorModal', ['$compile', 'serverContext', function ($compile, serverContext) {
 
         var link = function ($scope, element, attributes) {
@@ -30,11 +49,12 @@
     }]);
 
     /*
-     * Image Selector Button directive is a thin wrapper around
-     * Image Selector component. The actual Image Selector is not
-     * visible until the button marked with this directive is clicked
-     * at which point the Image Selector will be opened in a modal
-     * dialog.
+     * Image Selector Button directive is used together with the Image Selector Modal
+     * for the cases when the Image Selector should be opened on the button click and
+     * it should be open in a modal dialog.
+     *
+     * To use this directive set the the value of the sf-image-selector-button attribute
+     * to jQuery selector of the sf-image-selector-modal element.
      */
     selectors.directive('sfImageSelectorButton', function () {
 

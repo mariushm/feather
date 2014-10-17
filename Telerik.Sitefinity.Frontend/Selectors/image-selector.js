8693815afad39eb83bcbcd157de700e2ec7de224
osmak <iosmak@outlook.com>
2014-10-17 16:21:23 +0300

    Implemented blank sf-image-selector-button directive which is supposed to open the Image Selector and act as a bridge between selector and functionality needed to select an image.

---
+++ Telerik.Sitefinity.Frontend/Selectors/image-selector.js
@@ -0,0 +1,30 @@
+﻿; (function () {
+
+    var selectors = angular.module('selectors');
+
+    /*
+     * Image Selector Button directive is a thin wrapper around
+     * Image Selector component. The actual Image Selector is not
+     * visible until the button marked with this directive is clicked
+     * at which point the Image Selector will be opened in a modal
+     * dialog.
+     */
+    selectors.directive('sfImageSelectorButton', function () {
+
+        var link = function ($scope, element, attributes) {
+
+            $(element).bind('click', function () {
+                alert('Opening Image Selector...');
+            });
+
+        };
+
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+    });
+        
+
+}());
